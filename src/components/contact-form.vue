a1b42453daca847539f938c090ce57d7be1fc547
Brian Ford <brianfordcode@gmail.com>
2021-06-16 13:40:23 -0700

    add netlify contact form html

--- src/components/contact-form.vue
+++ src/components/contact-form.vue
@@ -1,5 +1,7 @@
 <template>
-<div class="form-container">
+<form name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
+  <input type="hidden" name="form-name" value="contact" />
+  
   <p>
     <label>Name: <input type="text" name="name" /></label>
   </p>
@@ -13,9 +15,7 @@
   </p>
   
 
-</div>
-
-  
+</form>
 
 
 
