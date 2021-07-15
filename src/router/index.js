cf1a8b4080b413f5fe5eac0cb9e08f3d16541f03
Brian Ford <brianfordcode@gmail.com>
2021-07-15 13:38:04 -0700

    add tab titles using meta, beforeEach in index.js

--- src/router/index.js
+++ src/router/index.js
@@ -4,35 +4,52 @@ const routes = [
   {
     path: '/',
     name: 'openingScreen',
-    component: () => import('../views/opening-screen.vue')
+    component: () => import('../views/opening-screen.vue'),
+    meta: {
+      title: "Portraits by Basundhara"
+    }
   },
   {
     path: '/about',
     name: 'About',
-    component: () => import('../views/About.vue')
+    component: () => import('../views/About.vue'),
+    meta: {
+      title: "About"
+    }
   },
   {
     path: '/contact',
     name: 'Contact',
-    component: () => import( '../views/Contact.vue')
+    component: () => import( '../views/Contact.vue'),
+    meta: {
+      title: "Contact"
+    }
   },
   {
     path: '/photoshoottips',
     name: 'PhotoshootTips',
-    component: () => import( '../views/PhotoshootTips.vue')
+    component: () => import( '../views/PhotoshootTips.vue'),
+    meta: {
+      title: "Photoshoot Tips"
+    }
   },
   {
     path: '/portfolio',
     name: 'Portfolio',
-    component: () => import( '../views/Portfolio.vue')
+    component: () => import( '../views/Portfolio.vue'),
+    meta: {
+      title: "Portfolio"
+    }
   },
   {
     path: '/testimonials',
     name: 'Testimonials',
-    component: () => import( '../views/Testimonials.vue')
+    component: () => import( '../views/Testimonials.vue'),
+    meta: {
+      title: "Testimonials"
+    }
   },
-  
-  
+
 ]
 
 const router = createRouter({
@@ -40,6 +57,9 @@ const router = createRouter({
   routes
 })
 
-
+router.beforeEach((to, from, next) => {
+  document.title = `${to.meta.title}`
+  next();
+})
 
 export default router
