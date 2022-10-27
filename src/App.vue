4d24a9f9106f9384f471696ecc87d495e59faee7
Brian Ford <brianfordcode@gmail.com>
2022-10-27 16:37:16 +0530

    working on package.json builds

--- src/App.vue
+++ src/App.vue
@@ -44,3 +44,32 @@
     }
   }
 </style>
+
+<!--
+  
+  --------- put this in package.json----------
+
+  ---- I don't know how to do npm run serve to get localhost: -----
+"scripts": {
+  "serve": "vue-cli-service --openssl-legacy-provider serve",
+  "build": "vue-cli-service --openssl-legacy-provider build",
+  "lint": "vue-cli-service --openssl-legacy-provider lint"
+},
+
+---- This works on netlify but not for localhost viewing: ------
+"scripts": {
+  "serve": "vue-cli-service",
+  "build": "vue-cli-service",
+  "lint": "vue-cli-service"
+},
+
+----- this works for localhost but not for netlify build: -----
+"scripts": {
+  "serve": "export NODE_OPTIONS=--openssl-legacy-provider && vue-cli-service serve",
+  "build": "export NODE_OPTIONS=--openssl-legacy-provider && vue-cli-service build",
+  "lint": "export NODE_OPTIONS=--openssl-legacy-provider && vue-cli-service lint"
+},
+
+
+
+-->
